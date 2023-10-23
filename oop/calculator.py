b34709425eccebfea827941d5e4d639461e0f710
unknown <info@tokman.com.ar>
2023-10-23 16:33:18 -0300

    More on calculator

--- oop/calculator.py
+++ oop/calculator.py
@@ -86,38 +86,3 @@ while True:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-class Estudiante:
-    def __init__(self, nombre, edad, grado):
-        self.nombre = nombre
-        self.edad = edad
-        self.grado = grado
-
-    def estudiar(self):
-        print(f"El estudiante {self.nombre} está estudiando")
-
-nombre = input("Nombre: ")
-edad = input("Edad: ")
-grado = input("Grado: ")
-
-estudiante = Estudiante(nombre, edad, grado)
-
-print(f"Name: {estudiante.nombre}")"""
